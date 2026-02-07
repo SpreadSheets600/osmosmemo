20b9c307e7513e023ac3892eaa738cde097c93cc
SpreadSheets600 <sohammaity239@gmail.com>
2026-02-07 07:27:51 +0000

    Add sync options to user settings for bookmarks synchronization

--- src/lib/utils/user-options.ts
+++ src/lib/utils/user-options.ts
@@ -4,18 +4,22 @@ export interface UserOptions {
   username: string;
   repo: string;
   filename: string;
+  syncToBookmarksBar: boolean;
+  bookmarksSyncIntervalMinutes: number;
 }
 
 export async function getUserOptions(): Promise<UserOptions> {
-  const options = await chrome.storage.sync.get(["accessToken", "tagOptions", "username", "repo", "filename"]);
+  const options = await chrome.storage.sync.get(["accessToken", "tagOptions", "username", "repo", "filename", "syncToBookmarksBar", "bookmarksSyncIntervalMinutes"]);
 
-  const { accessToken = "", username = "", repo = "", filename = "README.md", tagOptions = [] } = options;
+  const { accessToken = "", username = "", repo = "", filename = "README.md", tagOptions = [], syncToBookmarksBar = false, bookmarksSyncIntervalMinutes = 0 } = options;
   const safeOptions: UserOptions = {
     accessToken,
     username,
     repo,
     filename,
     tagOptions: tagOptions,
+    syncToBookmarksBar,
+    bookmarksSyncIntervalMinutes,
   };
 
   return safeOptions;
